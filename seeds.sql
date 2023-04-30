e6f304377920f87db3da9854d1c42075a2d72197
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-04-30 14:13:29 +0100

    add sql files

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (id, department_name)
+VALUES ("Finance"),
+       ("Human Resources"),
+       ("Engineering"),
+       ("Engineering"),
+       ("Compliance"),
+       ("Compliance"),
+       ("Executive Leadership")
+       ("Sales"),
+
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES ("Finance Manager", 60000),
+       ("Human Resources Officer", 35000),
+       ("Software Developer", 45000),
+       ("IT Technician", 30000),
+       ("Legal Associate", 45000),
+       ("Legal Director", 100000),
+       ("CEO", 350000),
+       ("Sales Executive", 40000);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ("Michelle", "Jackson"),
+       ("Finn", "Breen"),
+       ("100 Years of Solitude"),
+       ("Things Fall Apart"),
+       ("Crime and Punishment", 6),
+       ("Moby Dick"),
+       ("Decameron");
